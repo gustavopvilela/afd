bed63c6dab9a24ac5acfdf9fbeadb623a592afbe
gustavopvilela <gustavohp.vilela@gmail.com>
2025-04-19 17:30:01 -0300

    adição das funções de produto e complemento

---
+++ afd.py
@@ -0,0 +1,228 @@
+from copy import deepcopy
+from typing import *
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
+
+class AFD:
+    def __init__(self,
+                 estados: Iterable[str],
+                 alfabeto: Iterable[str],
+                 transicoes: Dict[Tuple[str, str], str],
+                 estado_inicial: str,
+                 estados_finais: Iterable[str]):
+
+        self.estados = set(estados)
+        self.alfabeto = set(alfabeto)
+        self.transicoes = dict(transicoes)
+        self.estado_inicial = estado_inicial
+        self.estados_finais = set(estados_finais)
+
+    def __str__(self):
+        return (
+            f"AFD(\n"
+            f"    estados: {self.estados}\n"
+            f"    alfabeto: {self.alfabeto}\n"
+            f"    transicoes:\n     "+
+            "\n     ".join([f"{k} -> {v}" for k, v in self.transicoes.items()]) + "\n"
+            f"    estado_inicial: {self.estado_inicial}\n"
+            f"    estados_finais: {self.estados_finais}\n"
+            f")"
+        )
+
+    @staticmethod
+    def inverter_dicionario (d):
+        return {v: k for k, v in d.items()}
+
+    def copiar (self):
+        return deepcopy(self)
+
+    def validar (self, cadeia: str):
+        estado_atual = self.estado_inicial
+
+        for simbolo in cadeia:
+            estado_atual = self.transicoes.get((estado_atual, simbolo))
+            if estado_atual is None:
+                return False
+
+        return estado_atual in self.estados_finais
+
+    @classmethod
+    def importar_jflap (cls, arquivo: str):
+        xml = ET.parse(arquivo)
+        afd = xml.getroot().find('automaton') # Acha a tag do AFD no XML
+
+        estados = set()
+        alfabeto = set()
+        transicoes = {}
+        finais = set()
+        inicial = None
+
+        # Mapeando IDs e nomes dos estados do AFD, já que nas tags de
+        # transição tem só o ID.
+        id_nome = {}
+        for estado in afd.findall('state'):
+            id, nome = estado.get('id'), estado.get('name')
+            id_nome[id] = nome
+
+            # Adicionando o estado no set de estados, caso não haja
+            estados.add(id_nome[id])
+
+            # Adicionando o estado no set de estados finais, caso ele seja
+            if estado.find('final') is not None:
+                finais.add(id_nome[id])
+
+            # Colocando o estado na variável de inicial, caso ele seja
+            if estado.find('initial') is not None:
+                inicial = id_nome[id]
+
+        # Encontrando as transições
+        for t in afd.findall('transition'):
+            from_id = t.find('from').text
+            to_id = t.find('to').text
+            simbolo = t.find('read').text
+
+            from_estado = id_nome[from_id]
+            to_estado = id_nome[to_id]
+
+            alfabeto.add(simbolo)
+            transicoes[(from_estado, simbolo)] = to_estado
+
+        return cls(estados, alfabeto, transicoes, inicial, finais)
+
+    def exportar_jflap (self, arquivo: str):
+        root = ET.Element('structure')
+
+        # Tipo do autômato: fa = finite automaton
+        type = ET.SubElement(root, 'type')
+        type.text = 'fa'
+
+        # Subtag dentro de structure: automaton
+        automaton = ET.SubElement(root, 'automaton')
+
+        # Para cada estado, colocamos como uma subtag
+        # Além disso, precisamos colocar id's únicos para cada estado, então
+        # vamos ter que criar novamente um dicionário que relaciona id com estado,
+        # assim, poderemos criar as transições depois.
+        id_nome = {}
+        id = 0
+        for estado in self.estados:
+            state = ET.SubElement(automaton, 'state')
+            state.set('name', estado)
+            state.set('id', str(id))
+            id_nome[id] = estado
+            id += 1
+
+            if self.estado_inicial == estado:
+                inicial = ET.SubElement(state, 'initial')
+
+            if estado in self.estados_finais:
+                final = ET.SubElement(state, 'final')
+
+        # Agora, vamos colocar as transições
+        for (ea, s), ed in self.transicoes.items():
+            # Para pegar os IDs a partir do valor, vamos inverter
+            # o dicionário
+            d_inv = self.inverter_dicionario(id_nome)
+
+            transicao = ET.SubElement(automaton, 'transition')
+            from_estado = ET.SubElement(transicao, 'from')
+            from_estado.text = str(d_inv[ea])
+
+            to_estado = ET.SubElement(transicao, 'to')
+            to_estado.text = str(d_inv[ed])
+
+            read = ET.SubElement(transicao, 'read')
+            read.text = s
+
+
+        # Formatando o arquivo com identação para facilitar a visualização
+        para_string = ET.tostring(root, 'utf-8')
+        dom = minidom.parseString(para_string)
+        arquivo_formatado = dom.toprettyxml(indent="    ")
+
+        with open(arquivo, 'w', encoding='utf-8') as f:
+            f.write(arquivo_formatado)
+
+    # Precisamos completar o autômato antes de chamar a função de complemento
+    # para garantir que funcione corretamente
+    def completar (self):
+        erro = 'E'
+        if erro not in self.estados:
+            self.estados.add(erro)
+
+        for q in list(self.estados_finais):
+            for a in self.alfabeto:
+                if (q, a) not in self.transicoes:
+                    self.transicoes[(q, a)] = erro
+
+        return self
+
+    def complemento (self):
+        for e in self.estados:
+            if e not in self.estados_finais:
+                self.estados_finais.add(e)
+            else:
+                self.estados_finais.remove(e)
+
+        return self
+
+    def produto (self, other, operacao):
+        operacoes_aceitas = ["uniao", "intersecao", "diferenca"]
+        if operacao not in operacoes_aceitas:
+            return None
+
+        afd1 = self
+        afd2 = other
+
+        if operacao == "diferenca":
+            afd2 = afd2.complemento()
+
+        produto_estados = {}
+
+        # Vamos mapear a junção dos estados, pois assim conseguiremos retornar o AFD corretamente
+        for estado_afd1 in afd1.estados:
+            for estado_afd2 in afd2.estados:
+                nome_estado = f"{estado_afd1}_{estado_afd2}"
+                produto_estados[(estado_afd1, estado_afd2)] = nome_estado
+
+        # Agora, vamos juntar os alfabetos dos dois AFDs
+        produto_alfabetos = afd1.alfabeto.union(afd2.alfabeto)
+
+        # Agora, vamos mapear as transições dos estados juntos de acordo com cada símbolo no novo alfabeto
+        produto_transicoes = {}
+
+        for (estado_afd1, estado_afd2), nome_estado_atual in produto_estados.items():
+            for simbolo in produto_alfabetos:
+                destino_afd1 = afd1.transicoes.get((estado_afd1, simbolo))
+                destino_afd2 = afd2.transicoes.get((estado_afd2, simbolo))
+
+                # Se as duas transições não levarem a None, então juntamos elas
+                if destino_afd1 is not None and destino_afd2 is not None:
+                    estado_destino = produto_estados.get((destino_afd1, destino_afd2))
+                    if estado_destino is not None:
+                        produto_transicoes[(nome_estado_atual, simbolo)] = estado_destino
+
+        # Definindo o novo estado inicial
+        produto_estado_inicial = produto_estados[(afd1.estado_inicial, afd2.estado_inicial)]
+
+        # Definindo os estados finais
+        produto_estados_finais = set()
+
+        for (estado_afd1, estado_afd2), nome_estado_atual in produto_estados.items():
+            is_final_afd1 = estado_afd1 in afd1.estados_finais
+            is_final_afd2 = estado_afd2 in afd2.estados_finais
+
+            if operacao == "uniao":
+                if is_final_afd1 or is_final_afd2:
+                    produto_estados_finais.add(nome_estado_atual)
+
+            if operacao in ("intersecao", "diferenca"):
+                # Se a operação for "diferença", o autômato já está invertido no começo da função
+                # então podemos usar somente o operador "and" que teremos os mesmos resultados
+                if is_final_afd1 and is_final_afd2:
+                    produto_estados_finais.add(nome_estado_atual)
+
+        # Definindo o nome dos estados para o retorno do AFD
+        nomes_estados = set(produto_estados.values())
+
+        return AFD(nomes_estados, produto_alfabetos, produto_transicoes, produto_estado_inicial, produto_estados_finais)
--- main.py
+++ main.py
@@ -1,133 +1,4 @@
-from typing import Iterable, Dict, Tuple
-import xml.etree.ElementTree as ET
-import xml.dom.minidom as minidom
-
-class AFD:
-    def __init__(self,
-                 estados: Iterable[str],
-                 alfabeto: Iterable[str],
-                 transicoes: Dict[Tuple[str, str], str],
-                 estado_inicial: str,
-                 estados_finais: Iterable[str]):
-
-        self.estados = set(estados)
-        self.alfabeto = set(alfabeto)
-        self.transicoes = dict(transicoes)
-        self.estado_inicial = estado_inicial
-        self.estados_finais = set(estados_finais)
-
-    def __str__(self):
-        return (
-            f"AFD(\n"
-            f"    estados: {self.estados}\n"
-            f"    alfabeto: {self.alfabeto}\n"
-            f"    transicoes:\n     "+
-            "\n     ".join([f"{k} -> {v}" for k, v in self.transicoes.items()]) + "\n"
-            f"    estado_inicial: {self.estado_inicial}\n"
-            f"    estados_finais: {self.estados_finais}\n"
-            f")"
-        )
-
-    @staticmethod
-    def inverter_dicionario(d):
-        return {v: k for k, v in d.items()}
-
-    @classmethod
-    def importar_jflap (cls, arquivo: str):
-        xml = ET.parse(arquivo)
-        afd = xml.getroot().find('automaton') # Acha a tag do AFD no XML
-
-        estados = set()
-        alfabeto = set()
-        transicoes = {}
-        finais = set()
-        inicial = None
-
-        # Mapeando IDs e nomes dos estados do AFD, já que nas tags de
-        # transição tem só o ID.
-        id_nome = {}
-        for estado in afd.findall('state'):
-            id, nome = estado.get('id'), estado.get('name')
-            id_nome[id] = nome
-
-            # Adicionando o estado no set de estados, caso não haja
-            estados.add(id_nome[id])
-
-            # Adicionando o estado no set de estados finais, caso ele seja
-            if estado.find('final') is not None:
-                finais.add(id_nome[id])
-
-            # Colocando o estado na variável de inicial, caso ele seja
-            if estado.find('initial') is not None:
-                inicial = id_nome[id]
-
-        # Encontrando as transições
-        for t in afd.findall('transition'):
-            from_id = t.find('from').text
-            to_id = t.find('to').text
-            simbolo = t.find('read').text
-
-            from_estado = id_nome[from_id]
-            to_estado = id_nome[to_id]
-
-            alfabeto.add(simbolo)
-            transicoes[(from_estado, simbolo)] = to_estado
-
-        return cls(estados, alfabeto, transicoes, inicial, finais)
-
-    def exportar_xml(self, arquivo: str):
-        root = ET.Element('structure')
-
-        # Tipo do autômato: fa = finite automaton
-        type = ET.SubElement(root, 'type')
-        type.text = 'fa'
-
-        # Subtag dentro de structure: automaton
-        automaton = ET.SubElement(root, 'automaton')
-
-        # Para cada estado, colocamos como uma subtag
-        # Além disso, precisamos colocar id's únicos para cada estado, então
-        # vamos ter que criar novamente um dicionário que relaciona id com estado,
-        # assim, poderemos criar as transições depois.
-        id_nome = {}
-        id = 0
-        for estado in self.estados:
-            state = ET.SubElement(automaton, 'state')
-            state.set('name', estado)
-            state.set('id', str(id))
-            id_nome[id] = estado
-            id += 1
-
-            if self.estado_inicial == estado:
-                inicial = ET.SubElement(state, 'initial')
-
-            if estado in self.estados_finais:
-                final = ET.SubElement(state, 'final')
-
-        # Agora, vamos colocar as transições
-        for (ea, s), ed in self.transicoes.items():
-            # Para pegar os IDs a partir do valor, vamos inverter
-            # o dicionário
-            d_inv = self.inverter_dicionario(id_nome)
-
-            transicao = ET.SubElement(automaton, 'transition')
-            from_estado = ET.SubElement(transicao, 'from')
-            from_estado.text = str(d_inv[ea])
-
-            to_estado = ET.SubElement(transicao, 'to')
-            to_estado.text = str(d_inv[ed])
-
-            read = ET.SubElement(transicao, 'read')
-            read.text = s
-
-
-        # Formatando o arquivo com identação para facilitar a visualização
-        para_string = ET.tostring(root, 'utf-8')
-        dom = minidom.parseString(para_string)
-        arquivo_formatado = dom.toprettyxml(indent="    ")
-
-        with open(arquivo, 'w', encoding='utf-8') as f:
-            f.write(arquivo_formatado)
+from afd import *
 
 if __name__ == '__main__':
     """
@@ -145,8 +16,21 @@ if __name__ == '__main__':
     print(novo_afd)
     """
 
-    file = "C:\\Users\\gusta\\Desktop\\7-1b.jff"
-    novo_afd = AFD.importar_jflap(file)
-    print(novo_afd)
+    file1 = "C:\\Users\\gusta\\Desktop\\a-impar.jff"
+    file2 = "C:\\Users\\gusta\\Desktop\\b-impar.jff"
 
-    AFD.exportar_xml(novo_afd, "C:\\Users\\gusta\\Desktop\\teste.jff")
+    a1 = AFD.importar_jflap(file1)
+    a2 = AFD.importar_jflap(file2)
+
+    print(a1)
+    print("=============================================")
+    print(a2)
+    print("=============================================")
+
+    operacoes_aceitas = ["uniao", "intersecao", "diferenca"]
+
+
+    a3 = AFD.produto(a1, a2, operacoes_aceitas[2])
+    print(a3)
+
+    AFD.exportar_jflap(a3, "C:\\Users\\gusta\\Desktop\\c.jff")
